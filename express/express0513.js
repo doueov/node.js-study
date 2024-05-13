c822b85c005d768c3059e7e7f266b68bf9a69fc0
gitdoyeon <gitpdyeon@gmail.com>
2024-05-13 11:07:24 +0900

    쿠키 데이터에 따라 삭제, 출력 실습

--- express/express0513.js
+++ express/express0513.js
@@ -4,6 +4,23 @@ const app = express();
 
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+    if(req.cookies.user) {
+        res.send("쿠키 데이터가 있습니다. /getUser를 이용하여 쿠키를 확인하세요.");
+    }else {
+        res.send("쿠키 데이터가 있습니다. /setUser를 이용하여 쿠키를 확인하세요.");
+    }
+});
+
+app.get("/deleteCookie", (req, res) => {
+    if(req.cookie.user) {
+        res.clearCookie("user");
+        res.redirect("/");
+    }else {
+        res.send("제거할 쿠키가 없습니다.")
+    }
+})
+
 app.get("/setUser", (req, res) => {
     res.cookie("user", {
         name: "kim",
