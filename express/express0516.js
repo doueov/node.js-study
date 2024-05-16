aa3bcf00fcecd4e326276a0b0484f630c397b070
gitdoyeon <gitpdyeon@gmail.com>
2024-05-16 15:44:54 +0900

    deleteSession error 헤결

--- express/express0516.js
+++ express/express0516.js
@@ -3,20 +3,20 @@ const app = express();
 const session = require('express-session');
 
 app.use(session({
-    secret : "AnimalBananaCandy"
+    secret: "AnimalBananaCandy"
 }));
 
 app.get("/setUser", (req, res) => {
-    req.session.user = {name: "kim", age: 20}
+    req.session.user = { name: "kim", age: 20 }
     res.redirect("/getUser");
 });
 
 app.get("/getUser", (req, res) => {
     res.send(req.session.user);
-})
+});
 
-app.get("deleteSession", (req, res) => {
-    if(req.session) {
+app.get("/deleteSession", (req, res) => {
+    if (req.session.user) {
         req.session.destroy(() => {
             res.redirect("/getUser");
         });
@@ -24,6 +24,11 @@ app.get("deleteSession", (req, res) => {
         res.send("제거할 세션이 없습니다.");
     }
 });
+
+// 1. /setUser/yoon => user name이 yoon으로 등록되게 라우터 만들기
+
+// 2. / => 루트 라우터일 때 세션이 있는지 안내하기
+
 
 app.listen(8889, () => {
     console.log("8889 포트입니다.");
