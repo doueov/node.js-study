f02af15376c05b9a5bfa493022df4ba3011e39ad
박도연 <121646938+gitdoyeon@users.noreply.github.com>
2024-06-24 12:02:57 +0900

    2024-06-24

--- express/hash/jwt.js
+++ express/hash/jwt.js
@@ -4,3 +4,23 @@ const jwt = reuqire('jsonwebtoken')
 const secret = "mySecretKey";
 
 // 페이로드 객체
+const payload = {
+    "id" : "1",
+    "username" : "john",
+    "roles" : "admin"
+};
+
+const token = jwt.sigh(payload, secret,
+    {expriesIn : '1h', algorithm : "HS256"});
+
+// 토큰 검증하기
+jwt.verify(token, secret, (err, decode) => {
+    if(err) {
+        console.error(err);
+        return;
+    }
+    // 디코딩된 페이로드 출력
+    console.error(decode);
+});
+
+console.log(decode);
