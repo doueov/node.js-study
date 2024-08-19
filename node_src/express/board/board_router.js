c22b8fab938271b35ef4c7c666fe0d64306de013
박도연 <121646938+gitdoyeon@users.noreply.github.com>
2024-08-19 10:55:23 +0900

    commit

---
+++ node_src/express/board/board_router.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+
+// 게시글 데이터
+let boardList = [];
+let numOfBoard = 1;
+
+// 게시글 API
+app.get('/', (req, res) => {
+    res.render('boardList', { "data": boardList });
+});
+
+app.get("/board", (req, res) => {
+    res.send(boardList);
+});
+
+app.post("/board", (req, res) => {
+    const board = {
+        "id": numOfBoard++,
+        "user_id": req.body.user_id,
+        "date": req.body.date,
+        "title": req.body.title,
+        "content": req.body.content
+    };
+    boardList.push(board);
+    res.send("글이 등록 되었습니다.");
+});
+
+app.delete("/board/:id", (req, res) => {
+    const findItem = boardList.find((item) => {
+        return item.id == req.params.id
+    });
+    const idx = boardList.indexOf(findItem);
+    boardList.splice(idx, 1);
+    res.send("글이 삭제되었습니다.");
+});
+
+app.put("/board/:id", (req, res) => {
+    const id = req.params.id;
+    const { title, content } = req.body;
+    const boardIndex = boardList.findIndex(board => board.id == id);
+    if (boardIndex !== -1) {
+        boardList[boardIndex] = {
+            ...boardList[boardIndex],
+            title: title || boardList[boardIndex].title,
+            content: content || boardList[boardIndex].content
+        };
+        res.send("글이 업데이트 되었습니다.");
+    }
+});
+
+module.exports = router;
