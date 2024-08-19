1c77913f2d7d9bef9e9a20c8c7606eec196b34f9
박도연 <121646938+gitdoyeon@users.noreply.github.com>
2024-08-19 12:02:17 +0900

    40개씩 -> 120개씩 나오도록 수정

--- puppeteer/puppeteer.js
+++ puppeteer/puppeteer.js
@@ -2,27 +2,37 @@ const puppeteer = require('puppeteer');
 
 async function getData() {
     try {
-        const brower = await puppeteer.launch({
+        const browser = await puppeteer.launch({
             headless: false
         });
-        const page = await brower.newPage();
+        const page = await browser.newPage();
         await page.goto("https://www.yes24.com/Product/Category/BestSeller?categoryNumber=001&pageNumber=1&pageSize=24");
-        
-        const booklist = await page.evaluate(()=> {
+
+        await page.select("select#pg_size", "120");
+        await page.waitForNavigation({ waitUntil: "networkidle0", timeout: 0 });
+
+        const booklist = await page.evaluate(() => {
             let books = [];
             let elements = document.querySelectorAll("#yesBestList > li");
             elements.forEach(elem => {
-                books.push(elem.querySelector("div.info_row.info_name > a.gd_name").innerText);
+                const book = {
+                    title: elem.querySelector("div.item_info > div.info_row.info_name > a.gd_name").innerText,
+                    rank: elem.querySelector(".ico.rank").innerText,
+                    img: elem.querySelector("em.img_bdr > img").getAttribute('src'),
+                    auth: elem.querySelector("div.item_info > div.info_row.info_pubGrp > span.info_auth").innerText,
+                    pub: elem.querySelector("div.item_info > div.info_row.info_pubGrp > span.info_pub").innerText, // 수정된 부분
+                };
+                books.push(book);
             });
             return books;
         });
         console.log("책 제목 :", booklist);
 
-        await brower.close();
+        await browser.close();
 
-    }
-    catch (error) {
+    } catch (error) {
         console.error(error);
     }
 }
+
 getData();
