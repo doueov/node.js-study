59e1a4de2098d61fe3682c6df7876e7a49eacb45
박도연 <121646938+gitdoyeon@users.noreply.github.com>
2024-08-19 10:45:21 +0900

    실행하면 naver페이지가 열리고 pdf 자동 다운됨

--- puppeteer/puppeteer.js
+++ puppeteer/puppeteer.js
@@ -1,13 +1,22 @@
 const puppeteer = require('puppeteer');
 (async () => {
     const brower = await puppeteer.launch({
-        headless: false
+        headless: false 
     });
     const page = await brower.newPage();
     await page.goto("https://www.naver.com");
-    page.setViewport();
+    page.setViewport({
+        width: 1920,
+        height: 1080,
+        deviceScaleFactor: 1
+    });
 
-    await page.screenshot({ path: 'example2.png' });
+    await page.screenshot({ path: 'example2.pdf' });
 
-    await brower.screenshot.close();
+    await page.pdf ({
+        path: "examplePDF.pdf",
+        format: "A4"
+    });
+
+    await page.close();
 })();
