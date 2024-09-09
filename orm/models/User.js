2181414bf99b16cb265f853edce8f287efe72dfa
박도연 <121646938+gitdoyeon@users.noreply.github.com>
2024-09-09 11:17:31 +0900

    user data

---
+++ orm/models/User.js
@@ -0,0 +1,18 @@
+modules.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("User", {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validator:{
+                notEmpty: true
+            }
+        },
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validator: {
+                notEmpty: true
+            }
+        }
+    });
+}
